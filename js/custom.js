fba4eeaba15cbe610bc83fc27ea3400837130a43
Muhammad Idrees <idreesiqbal104@gmail.com>
2024-04-01 01:25:03 +0500

    loader section add

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,11 @@
+// loader
+setTimeout(function() {
+  document.querySelector('.loader').classList.add('hide');
+}, 3000);
+
+// Strongly recommended: Hide loader after 20 seconds, even if the page hasn't finished loading
+setTimeout(hideLoader, 20 * 1000);
+
 // nav-bar
 function toggleMenu(x) {
   // icon animation
@@ -6,3 +14,36 @@ function toggleMenu(x) {
   // menu open and close
   document.querySelector(".sidenav").classList.toggle("active");
 }
+
+// owl-carousel-banner-icons
+$(function () {
+  // Owl Carousel
+  var owl = $(".owl-carousel");
+  owl.owlCarousel({
+    items: 8,
+    // margin: 5,
+    autoplay: true,
+    autoplayHoverPause: true,
+    autoplayTimeout: 2020,
+    smartSpeed: 2000,
+    animateIn: 'linear',
+    animateOut: 'linear',
+    loop: true,
+    nav: false,
+    dots: false,
+    // responsive: {
+    //   0: {
+    //     items: 1
+    //   },
+    //   425: {
+    //     items: 1
+    //   },
+    //   768: {
+    //     items: 2
+    //   },
+    //   1200: {
+    //     items: 8
+    //   }
+    // }
+  });
+});
