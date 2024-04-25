460d4f2dc31f7b8d88e53281b0df126417239959
Muhammad Idrees <idreesiqbal104@gmail.com>
2024-04-26 03:31:24 +0500

    services section half design complete

--- js/custom.js
+++ js/custom.js
@@ -15,7 +15,7 @@ function toggleMenu(x) {
 // owl-carousel-banner-icons
 $(function () {
   // Owl Carousel
-  var owl = $(".owl-carousel");
+  var owl = $(".owl-carousel-skills");
   owl.owlCarousel({
     items: 8,
     // margin: 5,
@@ -30,16 +30,49 @@ $(function () {
     dots: false,
     responsive: {
       0: {
-        items: 6
+        items: 2
       },
       300: {
-        items: 6
+        items: 2
       },
       768: {
-        items: 6
+        items: 4
       },
       1200: {
-        items: 8
+        items: 5
+      }
+    }
+  });
+});
+
+// owl-carousel-services
+$(function () {
+  // Owl Carousel
+  var owl = $(".owl-carousel-services");
+  owl.owlCarousel({
+    items: 5,
+    // margin: 5,
+    autoplay: true,
+    autoplayHoverPause: true,
+    autoplayTimeout: 2020,
+    smartSpeed: 2000,
+    animateIn: 'linear',
+    animateOut: 'linear',
+    loop: true,
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      300: {
+        items: 1
+      },
+      768: {
+        items: 3
+      },
+      1920: {
+        items: 5
       }
     }
   });
