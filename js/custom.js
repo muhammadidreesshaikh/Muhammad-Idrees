e4af5f6ee5ed9d8a4ebb4bf111415e73c704ae3f
Muhammad Idrees <idreesiqbal104@gmail.com>
2024-04-26 15:31:59 +0500

    services section resposnive complete

--- js/custom.js
+++ js/custom.js
@@ -69,9 +69,10 @@ $(function () {
         items: 2
       },
       768: {
-        items: 4
+        items: 3
       },
-      1920: {
+      // 1920
+      1200: {
         items: 5
       }
     }
